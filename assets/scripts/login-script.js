5244c86b8e2ab7bd45abe9625b62f084d4ca8391
@kbareee <akbar.github@gmail.com>
2024-02-17 14:20:12 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -18,12 +18,7 @@ const expectedPassword = 'superpassword';
 /* Comment : Menambahkan aksi klik pada button */
 loginFormElement.addEventListener('submit', function(event) {
     event.preventDefault();
-    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
-  
-    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
-       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
-       Jika tidak, maka panggil fungsi showPopUp().
-     */
+   
 
        /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan */
        const email = inputEmailElement.value;
